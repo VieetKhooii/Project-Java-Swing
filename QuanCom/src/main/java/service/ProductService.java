f1e41a72a3fc628942b38e5fb7fbf8272cbd5d10
VieetKhooii <117841805+VieetKhooii@users.noreply.github.com>
2023-04-21 09:40:22 +0700

    Add files via upload

---
+++ QuanCom/src/main/java/service/ProductService.java
@@ -0,0 +1,35 @@
+package service;
+
+import bus.ProductRepository;
+import model.Product;
+
+import java.util.List;
+
+public class ProductService {
+    public List<Product> getAllProduct(){
+        ProductRepository repository = new ProductRepository();
+        return repository.getAllProduct();
+    }
+
+    public boolean addProduct(String name, int amount, String unit, int price, int cateId){
+        ProductRepository repository = new ProductRepository();
+        return repository.addProduct(name, amount, unit, price, cateId) >= 1;
+    }
+
+    public boolean deleteProduct(int id){
+        ProductRepository repository = new ProductRepository();
+        return  repository.deleteProduct(id) >=1 ;
+    }
+
+    public boolean modifyProduct(
+            String name,
+            int amount,
+            String unit,
+            int price,
+            int cateId,
+            int productId
+    ){
+        ProductRepository repository = new ProductRepository();
+        return repository.modifyProduct(name, amount, unit, price, cateId, productId) >= 1;
+    }
+}
---
+++ QuanCom/src/main/java/service/UserService.java
@@ -0,0 +1,53 @@
+package service;
+
+import model.User;
+import bus.UserRepository;
+
+import java.util.List;
+
+public class UserService {
+
+    public List<User> getAllUsers(){
+        UserRepository userRepository = new UserRepository();
+        List<User> list = userRepository.getAllUser();
+        return list;
+    }
+    public boolean login(String email,String password){
+        UserRepository repository = new UserRepository();
+        return repository.login(email,password) >= 1;
+    }
+
+    public boolean addUser(String userName,
+                           String email,
+                           String password,
+                           int roleId){
+        UserRepository userRepository = new UserRepository();
+        return userRepository.addUser(
+                userName,
+                email,
+                password,
+                roleId) >= 1;
+    }
+
+    public boolean deleteUser(int id){
+        UserRepository userRepository = new UserRepository();
+        return userRepository.deleteUser(id) >= 1;
+    }
+
+    public boolean modifyUser(
+            int id,
+            String userName,
+            String email,
+            String password,
+            int roleId
+    ){
+        UserRepository userRepository = new UserRepository();
+        return  userRepository.modifyUser(
+                id,
+                userName,
+                email,
+                password,
+                roleId
+        ) >= 1;
+    }
+}
